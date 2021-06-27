f3323d18c3d411499ab01b393997f0f2a6eef319
Thaksina Jansaengsri <32285839+Thaksina@users.noreply.github.com>
2021-06-27 20:55:00 +0700

    Fixing layout

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -14,7 +14,7 @@ function Slide() {
       alt="First slide"
     />
     <Carousel.Caption>
-      <h3>First slide label</h3>
+      <h3>End Year Sale</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
@@ -28,7 +28,7 @@ function Slide() {
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h3>Second slide label</h3>
+      <h3>End Year Sale</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
@@ -41,7 +41,7 @@ function Slide() {
       alt="Third slide"
     />
     <Carousel.Caption>
-      <h3>Third slide label</h3>
+      <h3>End Year Sale</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
